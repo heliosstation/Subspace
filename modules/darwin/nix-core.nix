89d03dec8cd63e0aa3d2f9660119301951203b05
Suraj Bajirao Malode <helios@heliosstation.io>
2025-02-09 16:55:28 -0800

    Add configuration for nix-darwin

---
+++ modules/darwin/nix-core.nix
@@ -0,0 +1,20 @@
+{
+  ###################################################################################
+  #
+  #  Core configuration for nix-darwin
+  #
+  #  All the configuration options are documented here:
+  #    https://daiderd.com/nix-darwin/manual/index.html#sec-options
+  #
+  ###################################################################################
+
+  # Auto upgrade the nix-daemon service.
+  services.nix-daemon.enable = true;
+
+  # Disable auto-optimise-store because of this issue:
+  #   https://github.com/NixOS/nix/issues/7273
+  # "error: cannot link '/nix/store/.tmp-link-xxxxx-xxxxx' to '/nix/store/.links/xxxx': File exists"
+  nix.settings.auto-optimise-store = false;
+  nix.gc.automatic = false;
+  system.stateVersion = 5;
+}
