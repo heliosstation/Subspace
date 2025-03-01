53007e5c5903be1eeefc636f054a57ccc381000f
Suraj Bajirao Malode <helios@heliosstation.io>
2025-03-01 14:40:03 -0800

    fix: Update touchId sudo config name

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -11,7 +11,7 @@
 ###################################################################################
 {
   # Enable sudo authentication with Touch ID.
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   time.timeZone = "America/Los_Angeles";
 
